96850a91100da86dc9e57f768d5e1b8d8e9b00b3
HungNguyen1213 <52905968+HungNguyen1213@users.noreply.github.com>
2020-12-15 18:12:10 +0700

    trang chu xin xo

--- Kitucxa-frontend/src/main/java/kituc/web/HomeController.java
+++ Kitucxa-frontend/src/main/java/kituc/web/HomeController.java
@@ -1,6 +1,9 @@
 package kituc.web;
 
 import kituc.model.Thanhvien;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +15,35 @@ import org.springframework.web.client.RestTemplate;
 public class HomeController {
     RestTemplate rest = new RestTemplate();
     @GetMapping
-    public String login(Model model){
-        model.addAttribute("thanhvien",new Thanhvien());
+    public String show(Model model, HttpSession session){
+    	Thanhvien tv = (Thanhvien)session.getAttribute("thanhvien");
+    	if(tv!=null) {
+    		if(tv.getVitri().equals("admin"))return "trangchuadmin";
+    		else {
+    			model.addAttribute("sv", tv);
+    			return "trangchusinhvien";
+    		}
+    	}
+    	model.addAttribute("thanhvien",new Thanhvien());
         return "login";
     }
     @PostMapping
-    public String trangchu(@ModelAttribute Thanhvien thanhvien){
+    public String trangchu(Model model, @ModelAttribute Thanhvien thanhvien, HttpSession session){
         Thanhvien thanhvien1 = rest.postForObject("http://localhost:8080/thanhvien/login", thanhvien, Thanhvien.class);
-        if(thanhvien1==null) return "redirect:/";
-        else if(thanhvien1.getVitri().equals("admin"))return "trangchuadmin";
-        return "trangchusinhvien";
+        if(thanhvien1.getVitri().equals("admin")) {
+        	session.setAttribute("thanhvien", thanhvien1);
+        	return "trangchuadmin";
+        }
+        else if(thanhvien1.getVitri().equals("sinhvien")) {
+        	session.setAttribute("thanhvien", thanhvien1);
+        	model.addAttribute("sv", thanhvien1);
+        	return "trangchusinhvien";
+        }
+        return "login";
+    }
+    @GetMapping("/dang-xuat")
+    private String dangxuat(Model model, HttpSession session) {
+    	session.removeAttribute("thanhvien");
+    	return "redirect:/";
     }
 }
