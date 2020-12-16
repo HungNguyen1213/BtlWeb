a87034050a134e8c159ddd11320bef5ed30020f5
Huy <luuhuy01@gmail.com>
2020-12-16 21:25:24 +0700

    ket thuc bai tap

--- Kitucxa/src/main/java/kituc/data/PhongthueRepository.java
+++
@@ -1,9 +0,0 @@
-package kituc.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kituc.entity.Phongthue;
-
-public interface PhongthueRepository extends JpaRepository<Phongthue, Integer>{
-
-}
